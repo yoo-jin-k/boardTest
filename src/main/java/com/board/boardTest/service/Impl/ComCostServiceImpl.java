f4046ef6e053f55dac8709d3221c0468bffede3b
DESKTOP-LPKPFM8\mecca <dbwlso_811@meccatech.co.kr>
2023-05-25 14:57:07 +0900

    [ add ] - [ registerComCost annotation ] [   1. comCostServiceImpl registerComCost 주석달기 ]

--- src/main/java/com/board/boardTest/service/Impl/ComCostServiceImpl.java
+++ src/main/java/com/board/boardTest/service/Impl/ComCostServiceImpl.java
@@ -34,19 +34,31 @@ public class ComCostServiceImpl implements ComCostService {
 
     @Override
     public ComCostDTO registerComCost(ComCostDTO params) {
+
+        // custCd 부분 생성
         String id = "CUST_";
+        // 현재 날짜 가지고오기
         LocalDate localDate = LocalDate.now();
+        // 날짜를 "yyyMMdd" 형식으로 포맷
         String nowLocalData = localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        // 포맷된 날짜를 custCd에 추가
         id = id + nowLocalData;
 
+        // 동일한 등록일자로 기존 데이터를 검색하기 위한 comCostDTO 객체 생성
         ComCostDTO searchData = new ComCostDTO();
         searchData.setRegDt(nowLocalData);
+        // 동일한 등록일자를 가진 기존 ComCostDTO 객체의 총 개수 가져오기
         int i = comCostMapper.selectComCostTotalCount(searchData);
+        // 개수를 문자열로 변환하고 선행 0이 포함된 형식으로 변환
         id = id + "_" + Integer.toString(Integer.parseInt(String.format("%3d", i).trim()));
 
+        // 생성된 custCd를 params 객체에 설정
         params.setCustCd(id);
+
+        // comCostMapper를 사용하여 ComCostDTO 객체를 데이터베이스에 삽입
         comCostMapper.insertComCost(params);
 
+        // 새로운 ComCostDTO 객체 반환
         return new ComCostDTO();
     }
 
